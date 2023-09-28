e85531bd542220735dc48eb4f05e1eeb52d88cdb
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-28 12:14:25 +0200

    update

--- gallery_contentful/src/components/content/Content.jsx
+++ gallery_contentful/src/components/content/Content.jsx
@@ -1,4 +1,6 @@
 import { useFetch } from "../../hooks/fetch/useFetch";
+import style from "../content/Content.module.scss";
+
 
 export const Content = () => {
   const { data } = useFetch();
@@ -11,7 +13,7 @@ export const Content = () => {
             <header key={index}>
               <h2>{item.fields.galleryTitle}</h2>
               {item.fields.imageGallery?.map((item, index) => {
-                return <img src={`https:${item.fields.file.url}`} />;
+                return <img className= {style.Images} src={`https:${item.fields.file.url}`} />;
               })}
             </header>
           );
