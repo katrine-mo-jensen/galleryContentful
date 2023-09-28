03707addcabb5ce03b80154e909bdb9c06eb18de
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-28 11:39:21 +0200

    update

--- gallery_contentful/src/components/content/Content.jsx
+++ gallery_contentful/src/components/content/Content.jsx
@@ -1,7 +1,9 @@
 import { useFetch } from "../../hooks/fetch/useFetch";
+import style from "../content/Content.module.scss";
 
 export const Content = () => {
   const { data } = useFetch();
 
-  return <></>;
+  return <>
+  </>;
 };
