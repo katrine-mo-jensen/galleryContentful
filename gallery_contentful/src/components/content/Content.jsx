26b3f4f206defa9ed3e223f3047692ca9c8a5607
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-09-28 11:38:33 +0200

    gallery

--- gallery_contentful/src/components/content/Content.jsx
+++ gallery_contentful/src/components/content/Content.jsx
@@ -3,5 +3,20 @@ import { useFetch } from "../../hooks/fetch/useFetch";
 export const Content = () => {
   const { data } = useFetch();
 
-  return <></>;
+  return (
+    <>
+      <div>
+        {data?.items.map((item, index) => {
+          return (
+            <header key={index}>
+              <h2>{item.fields.galleryTitle}</h2>
+              {item.fields.imageGallery?.map((item, index) => {
+                return <img src={`https:${item.fields.file.url}`} />;
+              })}
+            </header>
+          );
+        })}
+      </div>
+    </>
+  );
 };
